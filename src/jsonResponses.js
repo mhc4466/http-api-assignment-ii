8849991beb2edff996ec8225c72babed619cdd51
mhc4466 <mhc4466@rit.edu>
2022-09-25 21:41:17 -0400

    Server routing on the side of server.js may be complete now

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,4 +1,27 @@
-const fs = require('fs');
+const users = {};
+
+const respondJSON = (request, response, status, content) => {
+    response.writeHead(status, {'Content-Type': 'application/json'});
+    response.write(JSON.stringify(content));
+    response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+    response.writeHead(status, {'Content-Type': 'application/json'});
+    response.end();
+};
+
+const getUsers = (request, response) => {
+    
+};
+
+const notReal = (request, response) => {
+
+};
+
+const notFound = (request, response) => {
+    respondJSONMeta(request, response, 404);
+};
 
 module.exports = {
 
--- src/server.js
+++ src/server.js
@@ -7,20 +7,76 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  '/': htmlHandler.getIndex,
-  '/style.css': htmlHandler.getCSS,
+    'GET': {
+        '/': htmlHandler.getIndex,
+        '/style.css': htmlHandler.getCSS,
+        '/getUsers': jsonHandler.getUsers,
+        '/notReal': jsonHandler.notReal
+    },
+    'HEAD': {
+        '/getUsers': jsonHandler.getUsersMeta,
+        '/notReal': jsonHandler.notRealMeta,
+        notFound: jsonHandler.notFound
+    },
+    'POST': {
+        '/addUser': jsonHandler.addUser
+    }
 };
 
+const parseBody = (request, response, callback) => {
+    const body = [];
+
+    request.on('error', (error) => {
+        console.dir(error);
+        response.statusCode = 400;
+        response.end();
+    });
+
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    request.on('end', () => {
+        const bodyString = Buffer.concat(body).toString();
+        const bodyObj = query.parse(bodyString);
+
+        callback(request, response, bodyObj);
+    });
+
+}
+
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
+    const parsedUrl = url.parse(request.url);
 
-  if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response);
-  }
+    //If user goes directly to URL, attempt a GET request
+    if (!urlStruct[request.method]) {
+        if (urlStruct['GET'][parsedUrl]) {
+            urlStruct['GET'][parsedUrl](request, response);
+        }
+        //If they go directly to a URL but it's not one handled by GET, then 404
+        else {
+            return urlStruct['HEAD'].notFound(request, response);
+        }
+    } 
 
-  response.end();
+    //An accepted request method is given
+
+    //If the method is POST, parse the body before giving it to the json handler
+    if (request.method === 'POST' && urlStruct['POST'][parsedUrl.pathname]) {
+        parseBody(request, response, urlStruct['POST'][parsedUrl.pathname])
+    }
+    //If it's any other accepted method, handle it normally
+    if (urlStruct[request.method][parsedUrl.pathname]) {
+        urlStruct[request.method][parsedUrl.pathname](request, response);
+    }
+    //Unaccepted method
+    else {
+        return urlStruct['HEAD'].notFound(request, response);
+    }
+
+    response.end();
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
