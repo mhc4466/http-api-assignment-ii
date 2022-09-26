b72d132b5a69b2424fa40a64b917d4ec7ba2d87a
mhc4466 <mhc4466@rit.edu>
2022-09-25 23:32:20 -0400

    Debugging perpetual status code 200

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,28 +1,83 @@
 const users = {};
 
 const respondJSON = (request, response, status, content) => {
-    response.writeHead(status, {'Content-Type': 'application/json'});
-    response.write(JSON.stringify(content));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(content));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-    response.writeHead(status, {'Content-Type': 'application/json'});
-    response.end();
+    console.log("meta");
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
 };
 
 const getUsers = (request, response) => {
-    
+    console.log("1");
+  const responseJSON = {
+    users,
+  };
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => {
+    console.log("2");
+  respondJSON(request, response, 200);
 };
 
 const notReal = (request, response) => {
+    console.log("3");
+  const responseJSON = {
+    id: 'notFound',
+    message: 'The page you are looking for could not be found',
+  };
+  respondJSON(request, response, 404, responseJSON);
+};
 
+const notRealMeta = (request, response) => {
+    console.log("4");
+  respondJSON(request, response, 404);
 };
 
 const notFound = (request, response) => {
-    respondJSONMeta(request, response, 404);
+    console.log("5");
+  respondJSONMeta(request, response, 404);
+};
+
+const addUser = (request, response, body) => {
+    console.log("6");
+  const responseJSON = {};
+  let status = 204;
+
+  console.dir(body);
+  console.dir(!body.name);
+  console.dir(body.age);
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    responseJSON.message = 'Missing name and/or age';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  if (!users[body.name]) {
+    status = 201;
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (status === 201) {
+    responseJSON.message = 'Created successfully';
+    return respondJSON(request, response, status, responseJSON);
+  }
+  return respondJSONMeta(request, response, status);
 };
 
 module.exports = {
-
+  getUsers,
+  getUsersMeta,
+  notReal,
+  notRealMeta,
+  notFound,
+  addUser,
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
@@ -7,76 +8,71 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    'GET': {
-        '/': htmlHandler.getIndex,
-        '/style.css': htmlHandler.getCSS,
-        '/getUsers': jsonHandler.getUsers,
-        '/notReal': jsonHandler.notReal
-    },
-    'HEAD': {
-        '/getUsers': jsonHandler.getUsersMeta,
-        '/notReal': jsonHandler.notRealMeta,
-        notFound: jsonHandler.notFound
-    },
-    'POST': {
-        '/addUser': jsonHandler.addUser
-    }
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notReal,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    '/notReal': jsonHandler.notRealMeta,
+    notFound: jsonHandler.notFound,
+  },
+  POST: {
+    '/addUser': jsonHandler.addUser,
+  },
 };
 
 const parseBody = (request, response, callback) => {
-    const body = [];
+  const body = [];
 
-    request.on('error', (error) => {
-        console.dir(error);
-        response.statusCode = 400;
-        response.end();
-    });
+  request.on('error', (error) => {
+    console.dir(error);
+    response.statusCode = 400;
+    response.end();
+  });
 
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
 
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        const bodyObj = query.parse(bodyString);
-
-        callback(request, response, bodyObj);
-    });
-
-}
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyObj = query.parse(bodyString);
+    callback(request, response, bodyObj);
+  });
+};
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
+  const parsedUrl = url.parse(request.url);
 
-    //If user goes directly to URL, attempt a GET request
-    if (!urlStruct[request.method]) {
-        if (urlStruct['GET'][parsedUrl]) {
-            urlStruct['GET'][parsedUrl](request, response);
-        }
-        //If they go directly to a URL but it's not one handled by GET, then 404
-        else {
-            return urlStruct['HEAD'].notFound(request, response);
-        }
-    } 
-
-    //An accepted request method is given
-
-    //If the method is POST, parse the body before giving it to the json handler
-    if (request.method === 'POST' && urlStruct['POST'][parsedUrl.pathname]) {
-        parseBody(request, response, urlStruct['POST'][parsedUrl.pathname])
+  // If user goes directly to URL, attempt a GET request
+  if (!urlStruct[request.method]) {
+    if (urlStruct.GET[parsedUrl]) {
+      urlStruct.GET[parsedUrl](request, response);
+    } else {
+      // If they go directly to a URL but it's not one handled by GET, then 404
+      urlStruct.HEAD.notFound(request, response);
     }
-    //If it's any other accepted method, handle it normally
-    if (urlStruct[request.method][parsedUrl.pathname]) {
-        urlStruct[request.method][parsedUrl.pathname](request, response);
-    }
-    //Unaccepted method
-    else {
-        return urlStruct['HEAD'].notFound(request, response);
-    }
+  }
 
-    response.end();
+  // An accepted request method is given
+
+  // If the method is POST, parse the body before giving it to the json handler
+  if (request.method === 'POST' && urlStruct.POST[parsedUrl.pathname]) {
+    parseBody(request, response, urlStruct.POST[parsedUrl.pathname]);
+  } else if (urlStruct[request.method][parsedUrl.pathname]) {
+    // If it's any other accepted method, handle it normally
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    // Unaccepted method
+    urlStruct.HEAD.notFound(request, response);
+  }
+
+  response.end();
 };
 
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
